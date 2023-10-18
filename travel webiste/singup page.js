237d3d23506cadb5ae5e541cb41b956c568e7686
vinyvinay <109907727+vinyvinay@users.noreply.github.com>
2023-10-19 00:53:46 +0530

    Delete travel webiste directory

--- travel webiste/singup page.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const container = document.getElementById('signup-container');
-    container.style.opacity = 1;
-    container.style.transform = 'translateY(0)';
-});
