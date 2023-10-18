84b88bf64e5520ee8bb305765f5b2df732fe6fb6
vinyvinay <109907727+vinyvinay@users.noreply.github.com>
2023-10-19 00:23:07 +0530

    Add files via upload

---
+++ travel webiste/singup page.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.getElementById('signup-container');
+    container.style.opacity = 1;
+    container.style.transform = 'translateY(0)';
+});
